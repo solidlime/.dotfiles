a9a669b448b6b5f64b5fc41c219afe0376f4facb
solidlime <solidlime@gmail.com>
2016-12-28 21:53:45 +0900

    sh fix

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sfn ~/dotfiles/.vimrc ~/.vimrc
+ln -sfn ~/dotfiles/.zshrc ~/.zshrc
+ln -sfn ~/dotfiles/.bashrc ~/.bashrc
+ln -sfn ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > a.sh
 sh ./a.sh
